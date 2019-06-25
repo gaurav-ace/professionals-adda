d844a2c922a555c02a553a59c16947e301b4ebb1
gaurav-ace <gaurav7101879@gmail.com>
2019-06-25 22:07:49 +0530

    deploying to heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const connectDB = require('./config/db')
+const path = require('path')
 const app = express()
 
 const PORT = process.env.PORT || 5000
@@ -14,16 +15,21 @@ app.use(
   })
 )
 
-app.get('/', (req, res) => {
-  res.send('API running')
-})
-
 //define routes
 app.use('/api/user', require('./routes/api/user'))
 app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/profile', require('./routes/api/profile'))
 app.use('/api/posts', require('./routes/api/posts'))
 
+//serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('client/build'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  })
+}
 app.listen(PORT, () => {
   console.log(`server is running on port:${PORT}`)
 })
